3f708b0c8c44e2d2b0764e484ded9f04c3cc759e
nadunc <>
2017-08-01 21:28:56 +0530

    Main menu created

--- MVN01/src/main/java/com/avn/mvn01/App.java
+++ MVN01/src/main/java/com/avn/mvn01/App.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.avn.mvn01;
 
 import com.avn.mvn01.controller.EmployeeController;
@@ -14,27 +13,73 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 /**
  *
  * @Author stephen.silver
  * @Document Main
  * @Created on 31/07/2017, 11:10:46 AM
-
+ *
  */
 public class App {
+        private static ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
     public static void main(String[] args) {
-        
-        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+//        EmployeeController employeeController = (EmployeeController) context.getBean("EmployeeController");
+//
+//        Employee emp = new Employee();
+//        emp.setName("Nadun");
+//        emp.setStatus(1);
+//        System.out.println(employeeController.addEmployee(emp));
+        Scanner sc = new Scanner(System.in);
+        boolean showMenu = true;
+        do {
+            System.out.println("------ Menu ------");
+
+            System.out.println("1. Insert employee");
+            System.out.println("2. Update employee");
+            System.out.println("3. Search employee");
+            System.out.println("99. Exit");
+
+            System.out.print("Enter selection : ");
+            String selection = sc.next();
+            switch (selection) {
+                case "1":
+                    insertEmplpoyee();
+                    break;
+                case "2":
+                    updateEmployee();
+                    break;
+                case "3":
+                    searchEmployee();
+                    break;
+                case "99":
+                    System.out.println("------ Thank you ------");
+                    showMenu = false;
+                    break;
+                default:
+                    System.out.println("Invalid input.");
+            }
+            System.out.println();
+        } while (showMenu);
+    }
+    
+    private static void insertEmplpoyee(){
+        System.out.println("Insert");
         
         EmployeeController employeeController = (EmployeeController) context.getBean("EmployeeController");
-
+//
         Employee emp = new Employee();
         emp.setName("Nadun");
         emp.setStatus(1);
         System.out.println(employeeController.addEmployee(emp));
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Menu");
+    }
+    private static void updateEmployee(){
+        System.out.println("Update");
+    }
+    private static void searchEmployee(){
+        System.out.println("Search");
     }
 }
